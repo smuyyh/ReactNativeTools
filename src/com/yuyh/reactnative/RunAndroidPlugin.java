9a2a0b9b78295611fe367c734c7a3003b72aa6a9
smuyyh <352091626@qq.com>
2016-12-01 12:22:19 +0800

    Initial commit

---
+++ src/com/yuyh/reactnative/RunAndroidPlugin.java
@@ -0,0 +1,45 @@
+package com.yuyh.reactnative;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.progress.util.StatusBarProgress;
+import com.intellij.openapi.project.Project;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by Kyrie.Y on 2016/12/1.
+ */
+public class RunAndroidPlugin extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String path = project.getBasePath();
+
+        String cmd = "cmd /c start react-native run-android";
+
+        Runtime run = Runtime.getRuntime();
+
+        try {
+            Process p = run.exec(cmd, null, new File(path));
+            BufferedInputStream in = new BufferedInputStream(p.getErrorStream());
+            BufferedReader inBr = new BufferedReader(new InputStreamReader(in));
+            String lineStr;
+            while ((lineStr = inBr.readLine()) != null) {
+                System.out.println(lineStr);
+            }
+
+            p.waitFor();
+            inBr.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
